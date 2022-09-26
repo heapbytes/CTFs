e2f1e2ed92643c3de0c07bf15fcd08689cd07552
heapbytes <heapbytes@pm.me>
2022-09-26 11:22:26 +0530

    Update solve.py

--- DownunderCTF-2022/blockchain/Solve-me/solve.py
+++ DownunderCTF-2022/blockchain/Solve-me/solve.py
@@ -22,7 +22,7 @@ account_from = {
                 "address":"0x9bbB1824338862cF04EE8Aa6E576c99b9C7E2964",
                 "private_key":"0x8bda9bf67b767d6a75a77eb109d3ec0813d465b6ae743a330282ef009c7a8479",
                 "balance":"2.0 ETH"
-    }#The instance for this challenge was destroyted, the private keys don't work anymore
+    }#The instance for this challenge was destroyed, the private keys won't work anymore
 
 challenge_url = 'https://blockchain-solveme-d0dfb4b964acf39d-eth.2022.ductf.dev/'
 contract_address = address = '0x6E4198C61C75D1B4D1cbcd00707aAC7d76867cF8'
